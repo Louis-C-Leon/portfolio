261125ed89e6d857cb09d066c0f6d757ec45bbc6
Louis Leon <louis@rivahq.com>
2020-02-29 11:58:05 -0800

    Rebuild for github pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src'),
-  output: { path: path.resolve(__dirname, 'build') },
+  output: { path: path.resolve(__dirname, 'docs') },
   module: {
     rules: [
       {
